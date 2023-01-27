4d37958062f76fc6ee0f962a97ef7c42c48d462c
Francis Jomer Gallardo <fjdgallardo@gmail.com>
2023-01-27 18:52:56 +0800

    Imported core module.

--- src/app/modules/todo/todo.module.ts
+++ src/app/modules/todo/todo.module.ts
@@ -2,20 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TodoRoutingModule } from './todo-routing.module';
-import { TodoListComponent } from './pages/todo-list/todo-list.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { TodoBoardComponent } from './components/todo-board/todo-board.component';
+import { CoreModule } from '@core/core.module';
 
 
 @NgModule({
   declarations: [
-    TodoListComponent,
     DashboardComponent,
     TodoBoardComponent
   ],
   imports: [
     CommonModule,
-    TodoRoutingModule
+    TodoRoutingModule,
+
+    // Custom modules go here
+    CoreModule
   ]
 })
 export class TodoModule { }
