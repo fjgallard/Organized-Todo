d0636f65a251337c6cea29487f1b66452ad033ed
Francis Gallardo <fjdgallardo@gmail.com>
2023-02-16 17:17:49 +0800

    Moved screen size service to core.

--- src/app/core/components/shell/shell.component.ts
+++ src/app/core/components/shell/shell.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ScreenSizeService } from '@utils/services/screen-size.service';
+import { ScreenSizeService } from '@core/services/screen-size/screen-size.service';
 
 @Component({
   selector: 'shared-shell',
